fedbf49fff242a74e8d790e1b0abb6da2f3315f3
Wojciech Kubaty <wkubaty@gmail.com>
2020-04-06 20:19:25 +0200

    Add layouts and navigation graph

---
+++ app/src/main/java/com/kubaty/catfacts/FactDetails.kt
@@ -0,0 +1,18 @@
+package com.kubaty.catfacts
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FactDetails : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_fact_details, container, false)
+    }
+
+}
